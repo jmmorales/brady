15005869f2ff9acaeaaf26538af36db52784b760
J.M. Morales <pajarom@gmail.com>
2020-05-18 09:24:58 +0100

    run B

---
+++ runB.R
@@ -0,0 +1,22 @@
+library(rstan)
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+C = as.matrix(read.csv('https://github.com/jmmorales/brady/raw/master/C.csv'))
+Ys = read.csv('https://github.com/jmmorales/lipids/raw/master/Y.csv')
+TT = as.matrix(read.csv('https://github.com/jmmorales/lipids/raw/master/TT.csv'))
+X = as.matrix(read.csv('https://github.com/jmmorales/brady/raw/master/X.csv'))
+
+dat <- list(N = 249,
+                Y = Y$Y,
+                total = Y$total,
+                X = X,
+                K = 4,
+                J = 36,
+                jj = Y$J_2,
+                L = 2,
+                TT = TT,
+                C = C,
+                ones = numeric(36) + 1)
+
+fit <- stan(file='stancode.stan', data = dat, iter = 10, chains = 1)
+save(fit, file="fitb.RData")
